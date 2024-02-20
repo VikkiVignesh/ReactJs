3d39d2273ba19752a1ed6e8bf8d2590e22ca3e68
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2024-02-20 23:13:53 +0530

    first reactapp

---
+++ first-app/src/components/App.js
@@ -0,0 +1,17 @@
+
+import '../components/App.css';
+import Header from "../components/header";
+import Contact from "../components/contact"
+import Feed from "../components/feedback"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Contact/>
+      <Feed />
+    </div>
+  );
+}
+
+export default App;
